af63dc4f4c4ea1efc82543b9bee35ab0131696a6
Leu Family <romanianfobjr@gmail.com>
2021-09-01 17:15:49 -0500

    middleware working

--- api/server.js
+++ api/server.js
@@ -11,6 +11,8 @@ server.use(express.json())
 const usersRouter = require('./users/users-router')
 server.use(logger)
 
+server.use('/api/users', usersRouter)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
